2313394c946842cd100d1c2308e4bd518bb66afc
Spark <79936503+spa5k@users.noreply.github.com>
2022-01-09 13:45:27 +0530

    Time: 52 ms (92.65%), Space: 14 MB (99.94%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set=set()
+        max_length = 0
+        left =0
+        for right in range(len(s)):
+                while s[right] in char_set:
+                    char_set.remove(s[left])
+                    left+=1
+                char_set.add(s[right])
+                max_length=max(max_length, right-left+1)
+        return max_length
