904791bfa537e3724cba3d487a73d1089d04b80d
Spark <79936503+spa5k@users.noreply.github.com>
2022-03-07 20:12:29 +0530

    Time: 93 ms (55.48%), Space: 13.9 MB (68.39%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,20 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry=0;
-        res=n=ListNode(0);
+        dummy = ListNode()
+        current = dummy
+
+        carry = 0
+
         while l1 or l2 or carry:
-            if l1:
-                carry +=l1.val
-                l1=l1.next;
-            if l2:
-                carry+=l2.val;
-                l2=l2.next;
-            carry,val=divmod(carry,10)
-            n.next=n=ListNode(val);
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            # new digit
+            val = v1+v2+carry
+
+            carry = val//10
+            val = val % 10
             
-        return res.next;
+
+            current.next = ListNode(val)
+            
+            # update pointers
+            current=current.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
