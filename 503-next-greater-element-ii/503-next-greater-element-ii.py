f55570f036bc012f7dc052e10dee4c8737ea4d70
Spark <79936503+spa5k@users.noreply.github.com>
2022-04-19 00:12:21 +0530

    Time: 222 ms (86.21%), Space: 15.5 MB (98.10%) - LeetHub

---
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [-1] * n
+
+        stack = []
+
+        for i in range(n * 2):
+            while stack != [] and nums[stack[-1]] < nums[i % n]:
+                res[stack.pop()] = nums[i % n]
+
+            if i < n:
+                stack.append(i)
+        return res
