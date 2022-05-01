b1545a46c576d3cc5aa43e8b185483db4ce7dab1
Spark <79936503+spa5k@users.noreply.github.com>
2022-05-01 22:49:34 +0530

    Time: 52 ms (77.60%), Space: 13.7 MB (97.93%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        # sort the array
+        nums.sort()
+
+        left = 0
+        right = len(nums)-1
+        res = []
+        while left <= right:
+            mid = (left+right) // 2
+            if nums[mid] >= target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        for i in range(left, len(nums)):
+            if nums[i] == target:
+                res.append(i)
+
+        return res
