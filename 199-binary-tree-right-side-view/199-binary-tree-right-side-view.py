10d13a18f74b1812d255449085367cd8fba79496
Spark <79936503+spa5k@users.noreply.github.com>
2022-07-10 23:49:45 +0530

    Time: 56 ms (35.32%), Space: 13.8 MB (97.81%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return root
+        res = []
+        q = collections.deque([root])
+
+        while q:
+            right_side = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    right_side = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if right_side:
+                res.append(right_side.val)
+        return res
