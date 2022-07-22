9a0543b89b8648f55474f743446c7733c3ef78b4
Spark <79936503+spa5k@users.noreply.github.com>
2022-07-22 16:22:57 +0530

    Time: 78 ms (5.41%), Space: 14.3 MB (14.60%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        queue = collections.deque([root])
+        res = []
+        even_level = False
+        while queue:
+            n = len(queue)
+            level = []
+            for i in range(n):
+                if even_level:
+                    # pop from right and append from left.
+                    node = queue.pop()
+                    # to maintain the order of nodes in the format of [left, right, left, right]
+                    # we push right first since we are appending from left
+                    if node.right:
+                        queue.appendleft(node.right)
+                    if node.left:
+                        queue.appendleft(node.left)
+                else:
+                    # pop from left and append from right
+                    node = queue.popleft()
+                    # here the order is maintained in the format [left, right, left, right]
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+                level.append(node.val)
+            res.append(level)
+            even_level = not even_level
+        return res
