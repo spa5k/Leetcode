619a7dde4498c81b46469ab96128e4fe78b086e6
Spark <79936503+spa5k@users.noreply.github.com>
2022-08-23 22:26:37 +0530

    Time: 15 ms (85.20%), Space: 21.3 MB (95.09%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -9,19 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return helper(nums, 0, nums.size()-1);
+  TreeNode *sortedArrayToBST(vector<int> &nums)
+  {
+    return helper(nums, 0, nums.size() - 1);
+  }
+
+  TreeNode *helper(vector<int> &nums, int left, int right)
+  {
+    if (left > right)
+    {
+      return nullptr;
     }
-    TreeNode* helper(vector<int>& nums, int low,int high){
-        if(low<=high){
-            int mid = low + (high-low)/2;
-            TreeNode* root = new TreeNode(nums[mid]);
-            root->left = helper(nums, low, mid-1);
-            root->right = helper(nums, mid+1, high);
-            return root;
-        }
-        return NULL;
-    }
+    int mid = (left + right) / 2;
+    TreeNode *temp = new TreeNode(nums[mid]);
+
+    temp->left = helper(nums, left, mid - 1);
+
+    temp->right = helper(nums, mid + 1, right);
+    return temp;
+  }
 };
