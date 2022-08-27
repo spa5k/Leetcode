41cc6b818b2da32ad04e1b08cefa26c6a22c3a27
Spark <79936503+spa5k@users.noreply.github.com>
2022-08-27 12:26:27 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.47%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,21 +1,13 @@
 class Solution
 {
 public:
-  int dp[20]{};
   int numTrees(int n)
   {
-    // numTrees[4] =  numTrees[0] * numTrees[3] +
-    //                numTrees[1] * numTrees[2] +
-    //                numTrees[2] * numTrees[1] +
-    //                numTrees[3] * numTress[1]
-    if (n <= 1)
-      return 1;
-    if (dp[n])
-      return dp[n];
-    for (int i = 1; i <= n; i++)
+    long ans = 1;
+    for (int i = 1; i < n; i++)
     {
-      dp[n] += numTrees(i - 1) * numTrees(n - i);
+      ans = ans * (n + i + 1) / i;
     }
-    return dp[n];
+    return ans / n;
   }
 };
