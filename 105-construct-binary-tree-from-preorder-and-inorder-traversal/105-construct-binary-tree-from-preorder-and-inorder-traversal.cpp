afa0a78459fa6db5a4640a6a78ffd1b088473ac7
Spark <79936503+spa5k@users.noreply.github.com>
2022-08-30 20:36:18 +0530

    Time: 39 ms (50.51%), Space: 26 MB (59.99%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+  int preIndex = 0;
+  TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+  {
+    return createTree(preorder, inorder, 0, inorder.size() - 1);
+  }
+
+  TreeNode *createTree(vector<int> &preorder, vector<int> &inorder, int start, int end)
+  {
+    if (start > end)
+    {
+      return nullptr;
+    }
+    TreeNode *node = new TreeNode(preorder[preIndex++]);
+    int pos;
+    for (int i = 0; i <= end; i++)
+    {
+      if (inorder[i] == node->val)
+      {
+        pos = i;
+        break;
+      }
+    }
+    node->left = createTree(preorder, inorder, start, pos - 1);
+    node->right = createTree(preorder, inorder, pos + 1, end);
+    return node;
+  }
+};
