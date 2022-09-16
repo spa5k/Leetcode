4defb9392d407aeffdbae7499092de01c61f891a
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-16 19:24:05 +0530

    Time: 3 ms (76.61%), Space: 8.4 MB (35.45%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -3,7 +3,10 @@ class Solution
 public:
   int minimumOperations(vector<int> &nums)
   {
-    unordered_set<int> s(nums.begin(), nums.end());
-    return s.size() - s.count(0);
+    unordered_set<int> s;
+    for (auto i : nums)
+      if (i != 0)
+        s.insert(i);
+    return s.size();
   }
 };
