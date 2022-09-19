fb9b32044cf56bc07660f3001f3ec277612ab52d
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-19 18:35:14 +0530

    Time: 102 ms (9.24%), Space: 15.3 MB (8.79%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+  int kthSmallest(vector<vector<int>> &matrix, int k)
+  {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (auto mat : matrix)
+    {
+      for (auto i : mat)
+      {
+        pq.push(i);
+      }
+    }
+    for (int i = 0; i < k-1; i++)
+    {
+      pq.pop();
+    }
+    return pq.top();
+  }
+};
