e17e8087b05f56dafc3e868aa06ed77e8728fedf
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-19 19:01:43 +0530

    Time: 127 ms (86.62%), Space: 47.1 MB (55.63%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,24 +1,26 @@
-class Solution {
+class Solution
+{
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-  int count = 0;
-  for (int i = 0; i < nums.size(); i++)
+  int findKthLargest(vector<int> &nums, int k)
   {
-    if (count < k)
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int count = 0;
+    for (int i = 0; i < nums.size(); i++)
     {
-      pq.push(nums[i]);
-      count++;
-    }
-    else
-    {
-      if (nums[i] > pq.top())
+      if (count < k)
       {
-        pq.pop();
         pq.push(nums[i]);
+        count++;
+      }
+      else
+      {
+        if (nums[i] > pq.top())
+        {
+          pq.pop();
+          pq.push(nums[i]);
+        }
       }
     }
+    return pq.top();
   }
-  return pq.top();
-    }
 };
