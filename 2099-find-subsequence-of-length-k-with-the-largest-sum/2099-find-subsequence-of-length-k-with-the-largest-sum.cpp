d2c9c747288f13ed8990928986bce4349444276c
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-22 00:56:18 +0530

    Time: 4 ms (96.25%), Space: 9.7 MB (38.75%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,33 +1,31 @@
-class Solution
-{
+class Solution {
 public:
-  vector<int> maxSubsequence(vector<int> &a, int k)
-  {
-    priority_queue<pair<int, int>> pq;
+    vector<int> maxSubsequence(vector<int>& a, int k) {
+        std::priority_queue<std::pair<int, int>> values;
 
-    for (auto i = 0; i < a.size(); ++i)
-    {
-      pq.emplace(a[i], i);
+        for(auto i = 0; i < a.size(); ++i) {
+            values.emplace(a[i], i);
+        }
+
+        std::vector<std::pair<int, int>> t;
+        t.reserve(k);
+
+        for(auto i = 0; i < k; ++i) {
+            t.push_back(values.top());
+            values.pop();
+        }
+
+        std::sort(t.begin(), t.end(), [](auto const& a, auto const& b) {
+            return a.second < b.second;
+        });
+
+        std::vector<int> results;
+        results.resize(k);
+
+        std::transform(t.begin(), t.end(), results.begin(), [](auto const& x) {
+            return x.first;
+        });
+
+        return results;
     }
-
-    vector<pair<int, int>> sorted;
-    sorted.reserve(k);
-
-    for (auto i = 0; i < k; ++i)
-    {
-      sorted.push_back(pq.top());
-      pq.pop();
-    }
-
-    sort(sorted.begin(), sorted.end(), [](auto const &a, auto const &b)
-         { return a.second < b.second; });
-
-    vector<int> results;
-    results.resize(k);
-
-    transform(sorted.begin(), sorted.end(), results.begin(), [](auto const &x)
-              { return x.first; });
-
-    return results;
-  }
 };
