cd09a82f7f38a4adfa039aecd64bbb4103ccfa63
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-23 00:29:24 +0530

    Time: 152 ms (42.44%), Space: 34.5 MB (42.89%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -3,37 +3,22 @@ class Solution
 public:
   int leastInterval(vector<char> &tasks, int n)
   {
-    if (n == 0)
-      return tasks.size();
-    unordered_map<char, int> m;
-    for (auto ch : tasks)
+    unordered_map<char, int> mp;
+    int count = 0;
+    for (auto e : tasks)
     {
-
-      m[ch]++; // adding the count of every character to the map
-    }
-    priority_queue<int> pq;
-    for (auto x : m)
-      pq.push(x.second);
-    int cycles = 0;
-    while (pq.empty() == false)
-    {
-      vector<int> temp;
-      for (int i = 0; i <= n; i++) // running our process for the cooldown process
-      {
-        if (pq.empty() == false)
-        {
-          temp.push_back(pq.top()); // we are seeing the tasks that we are processing during this cooldown pweriod
-          pq.pop();
-        }
-      }
-      for (int i : temp)
-      {
-        if (--i > 0)
-          pq.push(i);
-      }
-      cycles += (pq.empty() == true) ? temp.size() : n + 1;
+      mp[e]++;
+      count = max(count, mp[e]);
     }
 
-    return cycles;
+    int ans = (count - 1) * (n + 1);
+    for (auto e : mp)
+    {
+      if (e.second == count)
+      {
+        ans++;
+      }
+    }
+    return max((int)tasks.size(), ans);
   }
 };
