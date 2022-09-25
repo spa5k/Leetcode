714014c6de2f1287377af5ac15c56e40c3cd53e2
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-25 23:08:41 +0530

    Time: 119 ms (7.07%), Space: 12.3 MB (82.58%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,61 +1,32 @@
 class Solution
 {
 public:
-  int DR[4] = {1, 0, -1, 0};
-  int DC[4] = {0, -1, 0, 1};
-
-  bool valid_index(int i, int j, vector<vector<char>> &grid)
+  void dfs(int i, int j, vector<vector<char>> &grid)
   {
-    if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
-      return false;
-    return true;
+    if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0')
+      return;
+    grid[i][j] = '0';
+    dfs(i + 1, j, grid);
+    dfs(i - 1, j, grid);
+    dfs(i, j + 1, grid);
+    dfs(i, j - 1, grid);
   }
+
   int numIslands(vector<vector<char>> &grid)
   {
-    if (grid.size() == 0)
+
+    int islands = 0;
+    for (int i = 0; i < grid.size(); i++)
     {
-      return 0;
-    }
-    int n = grid.size();
-    int m = grid[0].size();
-    int no_of_islands = 0;
-    for (int i = 0; i < n; i++)
-    {
-      for (int j = 0; j < m; j++)
+      for (int j = 0; j < grid[0].size(); j++)
       {
         if (grid[i][j] == '1')
         {
-          no_of_islands++;
-          bfs(i, j, grid);
+          islands++;
+          dfs(i, j, grid);
         }
       }
     }
-    return no_of_islands;
-  }
-  void bfs(int r, int c, vector<vector<char>> &grid)
-  {
-    grid[r][c] = '0';
-    queue<pair<int, int>> q;
-    q.push({r, c});
-    while (!q.empty())
-    {
-      int i = q.front().first;
-      int j = q.front().second;
-      q.pop();
-      for (int k = 0; k < 4; k++)
-      {
-        int ci = DR[k] + i;
-        int cj = DC[k] + j;
-        if (!valid_index(ci, cj, grid))
-        {
-          continue;
-        }
-        if (grid[ci][cj] == '1')
-        {
-          q.push({ci, cj});
-          grid[ci][cj] = '0';
-        }
-      }
-    }
+    return islands;
   }
 };
