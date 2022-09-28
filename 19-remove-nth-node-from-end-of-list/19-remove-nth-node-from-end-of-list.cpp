cc61df25e002442fc016f94011e99cbe9f1c6685
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-28 20:38:58 +0530

    Time: 9 ms (39.46%), Space: 10.6 MB (75.00%) - LeetHub

---
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+  ListNode *removeNthFromEnd(ListNode *head, int n)
+  {
+    ListNode *fast = head;
+    ListNode *slow = head;
+
+    if (head == nullptr)
+    {
+      return nullptr;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+      fast = fast->next;
+    }
+
+    if (!fast)
+    {
+      return head->next;
+    }
+
+    while (fast->next)
+    {
+      fast = fast->next;
+      slow = slow->next;
+    }
+    slow->next = slow->next->next;
+    return head;
+  }
+};
