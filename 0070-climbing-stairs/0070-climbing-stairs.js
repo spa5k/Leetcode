3b90998797b2bbb20a00041107bbdabc7c00a633
Spark <79936503+spa5k@users.noreply.github.com>
2022-11-02 20:00:08 +0530

    Time: 85 ms (67.84%), Space: 41.9 MB (41.28%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = (n) => {
+  let one = 1;
+  let two = 1;
+
+  for (let i = 0; i < n-1; i++) {
+    const temp = one;
+    one += two;
+    two = temp;
+  }
+  return one;
+};
