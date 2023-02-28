35d70dacba36ad56a6fa574763b40add12d76723
Spark <79936503+spa5k@users.noreply.github.com>
2023-02-28 20:37:18 +0530

    Time: 35 ms (25.23%), Space: 23.9 MB (90.41%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,24 +5,18 @@ public:
   {
     int n = nums.size();
     vector<int> ans(n);
-    vector<int> left_Product(n);
-    vector<int> right_Product(n);
 
-    left_Product[0] = 1;
-    for (int i = 1; i < nums.size(); i++)
+    ans[n - 1] = 1;
+    for (int i = n - 2; i >= 0; i--)
     {
-      left_Product[i] = left_Product[i - 1] * nums[i - 1];
+      ans[i] = ans[i + 1] * nums[i + 1];
     }
 
-    right_Product[n - 1] = 1;
-    for (int j = n - 2; j >= 0; j--)
+    int leftProd = 1;
+    for (int i = 0; i < n; i++)
     {
-      right_Product[j] = right_Product[j + 1] * nums[j + 1];
-    }
-
-    for (int k = 0; k < n; k++)
-    {
-      ans[k] = left_Product[k] * right_Product[k];
+      ans[i] = leftProd * ans[i];
+      leftProd = leftProd * nums[i];
     }
 
     return ans;
