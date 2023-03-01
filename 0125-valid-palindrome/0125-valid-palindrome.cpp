75464fc3972a96c069bae4e6452e305d79dfc0b6
Spark <79936503+spa5k@users.noreply.github.com>
2023-03-01 23:57:00 +0530

    Time: 3 ms (93.58%), Space: 7.4 MB (60.56%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+  bool isPalindrome(string s)
+  {
+    int left = 0;
+    int right = s.size() - 1;
+    while (left < right)
+    {
+      while (!isalnum(s[left]) && left < right)
+      {
+        left++;
+      }
+      while (!isalnum(s[right]) && right > left)
+      {
+        right--;
+      }
+      if (tolower(s[left]) != tolower(s[right]))
+      {
+        return false;
+      }
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+};
