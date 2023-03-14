79fb72e78b3940d503c1a774aa93d7f7920ee201
Spark <79936503+spa5k@users.noreply.github.com>
2023-03-14 23:01:36 +0530

    Time: 37 ms (83.79%), Space: 19.1 MB (51.65%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+  vector<vector<int>> merge(vector<vector<int>> &intervals)
+  {
+    int n = intervals.size();
+    if (n == 1)
+    {
+      return intervals;
+    }
+
+    sort(intervals.begin(), intervals.end(), [](const auto &a, const auto &b)
+         { return a[0] < b[0]; });
+    vector<vector<int>> res;
+    int i = 0;
+    while (i < n - 1)
+    {
+      if (intervals[i][1] >= intervals[i + 1][0])
+      {
+        intervals[i + 1][0] = intervals[i][0];
+        intervals[i + 1][1] = max(intervals[i][1], intervals[i + 1][1]);
+      }
+      else
+      {
+        res.push_back(intervals[i]);
+      }
+      i++;
+    }
+    res.push_back(intervals[i]);
+    return res;
+  }
+};
