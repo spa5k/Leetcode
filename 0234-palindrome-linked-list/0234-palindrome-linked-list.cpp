7bc214815a0d8d39aa9b3e413092fcd2dd24e370
Spark <79936503+spa5k@users.noreply.github.com>
2023-04-09 16:59:05 +0530

    Time: 207 ms (93.92%), Space: 114.1 MB (93.43%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode *slow = head, *fast = head, *prev, *temp;
+
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        prev = slow;
+        slow = slow->next;
+        prev->next = NULL;
+        while (slow)
+        {
+            ListNode *temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+        ListNode *left = head;
+        ListNode *right = prev;
+
+        while (right)
+        {
+            if (left->val != right->val)
+            {
+                return false;
+            }
+            left = left->next;
+            right = right->next;
+        }
+        return true;
+    }
+};
