778c81870a4fd25602832d90f915c4a9a8a5d18e
Spark <79936503+spa5k@users.noreply.github.com>
2023-04-10 13:19:12 +0530

    Time: 9 ms (19.21%), Space: 9.6 MB (80.14%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            int midn = nums[mid];
+
+            if (midn == target)
+            {
+                return mid;
+            }
+            else if (midn < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
