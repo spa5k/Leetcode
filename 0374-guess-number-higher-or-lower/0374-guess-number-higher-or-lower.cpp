59d752ff3c0d3a49d5edb8bfdaecb0b167022753
Spark <79936503+spa5k@users.noreply.github.com>
2023-04-10 13:28:19 +0530

    Time: 0 ms (100.00%), Space: 6 MB (57.12%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -0,0 +1,30 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution
+{
+public:
+    int guessNumber(int n)
+    {
+        int start = 1, end = n;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            int pos = guess(mid);
+            if (pos == 0)
+                return mid;
+            if (pos == -1)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return -1;
+    }
+};
