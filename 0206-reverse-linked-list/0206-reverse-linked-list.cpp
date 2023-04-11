7d5991e9d804f63b84be3c298b172a9a3bd529a5
Spark <79936503+spa5k@users.noreply.github.com>
2023-04-11 18:27:20 +0530

    Time: 3 ms (95.48%), Space: 8.4 MB (52.51%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+
+        ListNode *reverse = NULL;
+        ListNode *curr = head;
+
+        while (curr != NULL)
+        {
+            ListNode *next = curr->next;
+            curr->next = reverse;
+            reverse = curr;
+            curr = next;
+        }
+
+        return reverse;
+    }
+};
