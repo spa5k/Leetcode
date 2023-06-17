c1706fc2e8e3b4806363b46c18f4339b71e3a132
Spark <79936503+spa5k@users.noreply.github.com>
2023-06-17 19:03:03 +0530

    Time: 66 ms (64.67%), Space: 46.3 MB (48.34%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let minLength = Infinity; // Initialize the minimum length as infinity
+  let sum = 0; // Initialize the current sum
+  let left = 0; // Left pointer of the sliding window
+
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right]; // Expand the window by adding the current element
+
+    while (sum >= target) {
+      // Shrink the window as much as possible while the sum is still >= target
+      minLength = Math.min(minLength, right - left + 1); // Update the minimum length
+      sum -= nums[left]; // Remove the leftmost element from the sum
+      left++; // Move the left pointer to the right
+    }
+  }
+
+  return minLength === Infinity ? 0 : minLength;
+};
+
