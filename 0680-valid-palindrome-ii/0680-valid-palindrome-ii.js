6afed6539228609ec754f763297c4a2ca814034d
Spark <79936503+spa5k@users.noreply.github.com>
2023-06-18 08:30:35 +0530

    Time: 77 ms (73.96%), Space: 47.9 MB (71.06%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return (
+        isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)
+      );
+    }
+    left++, right--;
+  }
+  return true;
+};
+
+function isPalindrome(str, left, right) {
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++, right--;
+  }
+  return true;
+}
