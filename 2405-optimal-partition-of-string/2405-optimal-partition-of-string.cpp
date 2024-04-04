988d6e8cd21e65022fb49f09004d5e76c7d1bcec
Spark <79936503+spa5k@users.noreply.github.com>
2024-04-04 19:26:58 +0530

    Time: 152 ms (51.20%), Space: 46.8 MB (49.00%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int partitionString(string s)
+    {
+        unordered_set<char> st;
+        int count = 1;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (st.find(s[i]) != st.end())
+            {
+                count++;
+                st.clear();
+            }
+            st.insert(s[i]);
+        }
+        return count;
+    }
+};
