61c1d9a3c9c331a9d974c2b960c5fc7ae5c3d326
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-11 22:19:16 +0530

    Time: 68 ms (59.29%), Space: 51.8 MB (92.21%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -1,17 +1,17 @@
 var combinationSum2 = function (candidates, target) {
     const result = [];
     const state = [];
-    const memo = {};
-    candidates.sort();
+    const memo = new Set();
+
+    candidates.sort((a, b) => a - b);
 
     function dfs(start, sum) {
-        if (sum === target && !memo[state.join('')]) {
-            result.push([...state]);
-            memo[state.join('')] = true;
-
-            return;
-        }
-        if (memo[state.join('')]) {
+        if (sum === target) {
+            const combination = state.slice().join(',');
+            if (!memo.has(combination)) {
+                result.push([...state]);
+                memo.add(combination);
+            }
             return;
         }
 
@@ -20,12 +20,15 @@ var combinationSum2 = function (candidates, target) {
         }
 
         for (let i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] === candidates[i - 1]) {
+                continue;
+            }
             state.push(candidates[i]);
             dfs(i + 1, sum + candidates[i]);
             state.pop();
         }
-        memo[state.join('')] = true;
     }
+
     dfs(0, 0);
     return result;
 };
