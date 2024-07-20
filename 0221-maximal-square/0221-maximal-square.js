29a3af311417a1ec41c9c1da0d4c69eef6a47411
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-20 20:40:36 +0530

    Time: 119 ms (21.78%), Space: 58.2 MB (37.01%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.js
+++ 0221-maximal-square/0221-maximal-square.js
@@ -5,31 +5,34 @@
 var maximalSquare = function (matrix) {
     if (!matrix) return 0;
 
-    const m = matrix.length;
-    const n = matrix[0].length;
+    const rows = matrix.length;
+    const cols = matrix[0].length;
 
     let res = 0;
 
-    const dp = Array(m)
+    const dp = Array(rows)
         .fill()
-        .map(() => Array(n).fill(0));
+        .map(() => Array(cols).fill(0));
 
-    for (let r = 0; r < m; r++) {
+    for (let r = 0; r < rows; r++) {
         dp[r][0] = matrix[r][0];
         res = Math.max(dp[r][0], res);
     }
 
-    for (let c = 0; c < n; c++) {
+    for (let c = 0; c < cols; c++) {
         dp[0][c] = matrix[0][c];
         res = Math.max(dp[0][c], res);
     }
 
-    for (let r = 1; r < m; r++) {
-        for (let c = 1; c < n; c++) {
+    for (let r = 1; r < rows; r++) {
+        for (let c = 1; c < cols; c++) {
             if (matrix[r][c] == 0) continue;
 
-            dp[r][c] =
-                Math.min(dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1]) + 1;
+            let left = dp[r][c - 1];
+            let up = dp[r - 1][c];
+            let diagonal = dp[r - 1][c - 1];
+
+            dp[r][c] = Math.min(left, up, diagonal) + 1;
             res = Math.max(dp[r][c], res);
         }
     }
