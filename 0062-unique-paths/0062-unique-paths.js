c41e51c924552180d6e10ac30b38b1493dfab472
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-20 13:54:18 +0530

    Time: 61 ms (25.04%), Space: 49.3 MB (45.20%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+    const dp = [];
+    for (let i = 0; i < n; i++) {
+        const mArray = new Array(m).fill(1);
+        dp.push([...mArray]);
+    }
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < m; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+
+    return dp[n - 1][m - 1];
+};
