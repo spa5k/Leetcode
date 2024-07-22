4008dcd86962dc60e723663887016b29408cd94f
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-22 18:17:38 +0530

    Time: 138 ms (62.89%), Space: 50.7 MB (55.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -1,3 +1,32 @@
+function generateSubsets(nums) {
+    const res = [[]];
+    const state = [];
+
+    function dfs(start) {
+        if (start === nums.length) {
+            return;
+        }
+
+        state.push(nums[start]);
+        res.push([...state]);
+        dfs(start + 1);
+        state.pop();
+        dfs(start + 1);
+    }
+
+    dfs(0);
+    return res;
+}
+
+function isIncreasing(nums) {
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i - 1] >= nums[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 /**
  * @param {number[]} nums
  * @return {number}
