f8ffe37784760cc4d1c51d7c4c2a1ad727f8fe4f
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-29 16:33:53 +0530

    Time: 135 ms (66.41%), Space: 50.5 MB (64.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -5,15 +5,14 @@
 var lengthOfLIS = function (nums) {
     if (nums.length === 0) return 0;
 
-    let dp = new Array(nums.length).fill(1);
+    const dp = new Array(nums.length).fill(1);
 
-    for (let i = nums.length - 1; i >= 0; i--) {
+    for (let i = nums.length; i >= 0; i--) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] < nums[j]) {
                 dp[i] = Math.max(dp[i], 1 + dp[j]);
             }
         }
     }
-
     return Math.max(...dp);
 };
