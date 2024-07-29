c103f804f20643d1f038c10dd6610777ba8457c4
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-29 23:23:15 +0530

    Time: 149 ms (72.37%), Space: 52.5 MB (69.97%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.js
+++ 0279-perfect-squares/0279-perfect-squares.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var numSquares = function (n) {
- const dp = Array(n + 1).fill(Number.MAX_VALUE);
+    const dp = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
     dp[0] = 0;
     for (let i = 1; i * i <= n; i++) {
-        const cur = i * i;
+        let cur = i * i;
         for (let j = cur; j <= n; j++) {
             dp[j] = Math.min(dp[j], dp[j - cur] + 1);
         }
