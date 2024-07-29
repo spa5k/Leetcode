9dfd5898cdb0b0d10f9f185bf74e84815aebd962
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-29 17:42:53 +0530

    Time: 69 ms (77.73%), Space: 52.3 MB (81.87%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+    let sign = '+';
+    const strArray = x.toString().split('');
+    if (strArray[0] === '-') {
+        sign = '-';
+        strArray.shift();
+    }
+
+    const str = parseInt(strArray.reverse().join(''));
+        if (str > Math.pow(2, 31)) return 0;
+
+    if (sign === '-') {
+        return -str;
+    }
+    return str;
+};
